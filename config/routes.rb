9012d2eb83681f514fe99b3581ebcf36fd32ccdb
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-29 17:54:26 +0100

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  get "testing", to: "pages#testing"
+
+
 end
