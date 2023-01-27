ddd3b38af339923d1f2417463f5dd5e03b4fcb17
Gabriel Ardevol <artsdevol@gmail.com>
2023-01-27 12:28:44 +0100

    backend and small frontend changes

--- app/controllers/text_cat.rb
+++ app/controllers/text_cat.rb
@@ -2,7 +2,7 @@ HISTORY_CAT =
 [
   { section: "Història", title: "Medieu", content: "La història vitivinícola del Priorat es remunta a fa més de vuit segles, quan uns monjos provinents de la Provença francesa van assentar una cartoixa als peus del Montsant, en l’actual Escaladei. Consta que l’any 1194 s’estableixen les primeres vinyes en el sòl de llicorella, la característica pissarra prioratina. Al llarg d’aquests segles de viticultura eclesiàstica s’introduiren varietats de raïm com la carinyena o la garnatxa, que encara romanen en la regió avui dia.
     Sota el control de la Cartoixa es trobaven els nou pobles del que avui coneixem com a Priorat antic, d’entre ells Porrera.
-    ".html_safe , image: "history/1.jpg"},
+    ".html_safe , image: "history/1.jpg", href: "/history"},
   { section: "Història", title: "Modernitat",
     content: "Amb la política agrícola de la desamortització de Mendizàbal, el 1836, es van suprimir els terrenys eclesiàstics i moltes de les vinyes cartoixanes van quedar descuidades, creixent sense guia. La producció entraria aquí en un declivi, rotundament agreujat a finals del XIX amb l’arribada de la coneguda fil·loxera, insecte que destruïa les arrels del cep. Com els avantpassats de la família Ardèvol, molts agricultors van substituïr el conreu de la vinya per d’altres com l’avellana i l’ametlla.
     En els primers anys del segle XX, la viticultura del Priorat va continuar en declivi i va ser eclipsada per altres regions vitivinícoles d'Espanya.
@@ -18,7 +18,7 @@ ABOUT_CAT =
   {section: "Nosaltres", title: "2000-2020", content: "Som una petita empresa familiar fundada per Josep Ardèvol, viticultor i pare. Ell va començar amb la replantació de la vinya familiar al 1995, durant les primeres etapes del boom vitivinícola de la regió. Quatre anys després començaria a produir el vi que ara coneixem, a partir de la fundació del setè celler de Porrera juntament amb el seu germà Albert i la Roser Galceràn com a socis, i la Raquel en l’administració.
     la roser amorós(2003) i el ricard(200X), quan s’afegeixen?
     Gràcies a aquest projecte, el nostre pare va poder viatjar arreu del món i conèixer a moltes persones, de les quals moltes l’acompanyarien durant tota la vida. Amb la seva peculiar manera de fer i la seva proximitat, des del petit poble de Porrera va obrir-nos una finestra al món.
-    ".html_safe , image: "people/1.jpg"},
+    ".html_safe , image: "people/1.jpg", href: "/about"},
   {section: "Nosaltres", title: "Actualitat", content: " La nostra identitat és prioratina, austera com la mateixa llicorella: som una empresa petita i familiar, i allò que coneixem bé és la terra, el raïm i el vi. Aquest és el producte que genuïnament oferim, sempre acompanyats per la bona enologia.
     Actualment la empresa la portem la Raquel i els fills, distribuïts de forma que el Max s’encarrega de la viticultura i del celler,la Niru? l’Albert l’ajuda, a Raquel s’encarrega de l’administració i la direcció, de la coordinació i de fer de mare i el Gabriel de la web i poc a poc de la identitat de marca. també seguim comptant amb el suport de la Roser Amorós com a enòloga i el Ricard Zamora com a comercial.
     L’Anjoli pren el relleu de la Roser com a socia corporativa. I seguim comptant també amb tanta altra gent del poble i de la comarca que d’una forma o altra ens presten el seu suport, amb el cooperativisme que ens identifica com a prioratins. No estic segur d’això del cooperativisme, ho he escrit perquè em sona bé
@@ -38,7 +38,7 @@ WINES_CAT =
 	nombre de botes: 100<br>
 	tipus de fusta: roure francès ‘allier’<br>
   ".html_safe,
-  image: "bottles/1.JPG",
+  image: "bottles/1.JPG", href: "/wines"
 
 },
   {
