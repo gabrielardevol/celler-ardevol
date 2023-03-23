b79da2fbb7690a88ca602dc0241d6ff972b094c0
Gabriel Ardevol <artsdevol@gmail.com>
2023-03-23 13:33:24 +0100

    several improvements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   HISTORY_CAT =
 [
-  { section: "Història", title: "Medieu", content: "
+  { section: "Ardèvol, Porrera, Priorat", title: "Medieu", content: "
     La història del vi del Priorat es remunta a fa més de vuit segles, quan uns monjos provinents de la Provença francesa van assentar la Cartoixa de Scala Dei als peus del Montsant, en l’actual Escaladei. Consta que l’any 1194 s’estableixen les primeres vinyes en el sòl de llicorella, la característica pissarra prioratina. Durant aquests segles de viticultura litúrgica s'introduïren varietats de raïm com la carinyena o la garnatxa, que encara romanen en la regió avui dia. Sota el domini dels cartoixans es trobaven els nou pobles del que avui coneixem com a Priorat antic, d’entre ells Porrera.    ".html_safe , image: "history/1.jpg", href: "/history"},
   { section: "Història", title: "Modernitat",
     content: "
@@ -14,8 +14,10 @@ El caràcter dels vins prioratins es deu al sòl de pissarra llicorella, pobre e
 
 ABOUT_CAT =
 [
-  {section: "Nosaltres", title: "2000-2020", content: "
-    Som una petita empresa familiar fundada per Josep Ardèvol, viticultor i també pare. Ell va començar amb la replantació de la vinya familiar al 1995, quan el Priorat començava a valorar-se com a regió vinícola. Quatre anys després començava a produir el vi que ara coneixem, a partir de la fundació del celler juntament amb el seu germà Albert i la Roser Galceràn com a socis, i la Raquel en l’administració, i un poc més tard la Roser Amorós com a enòloga i el Ricard Zamora en l’apartat comercial. Gràcies a aquest projecte, el nostre pare va poder viatjar arreu del món i conèixer a moltes persones, de les quals moltes l’acompanyarien durant tota la vida. Amb la seva peculiar manera de fer i la seva proximitat, des del petit poble de Porrera va obrir-nos una finestra al món. Actualment el Celler Ardèvol és el setè més antic del poble.
+  {section: "Nosaltres", title: "2000-2020",
+
+    content: "
+    Som una petita empresa familiar fundada per Josep Ardèvol, viticultor i també pare. Ell va iniciar amb la replantació de la vinya familiar al 1995, quan el Priorat començava a valorar-se com a regió vinícola. Quatre anys després començava a produir el vi que ara coneixem, a partir de la fundació del celler juntament amb el seu germà Albert i la Roser Galceràn com a socis, i la Raquel en l’administració, i un poc més tard la Roser Amorós com a enòloga i el Ricard Zamora en l’apartat comercial. Gràcies a aquest projecte, el nostre pare va poder viatjar arreu del món i conèixer a moltes persones, de les quals moltes l’acompanyarien durant tota la vida. Amb la seva peculiar manera de fer i la seva proximitat, des del petit poble de Porrera va obrir-nos una finestra al món. Actualment el Celler Ardèvol és el setè més antic del poble.
     ".html_safe , image: "people/1.jpg", href: "/about"},
   {section: "Nosaltres", title: "Actualitat", content: "
     La nostra identitat és prioratina, austera com la mateixa llicorella: som una empresa petita i familiar, i allò que coneixem bé és la terra, el raïm i el vi. Aquest és el producte que genuïnament oferim, acompanyats sempre per la bona enologia. Actualment, l'empresa la portem la Raquel i els fills, distribuïts de forma que el Max s’encarrega de la viticultura i del celler amb el suport de l’Albert i la Niruta, la Raquel s’encarrega de l’administració i la direcció, de la coordinació i de fer de mare i el Gabriel de la web i de la identitat de marca. També seguim comptant amb el suport de la Roser Amorós com a enòloga i el Ricard Zamora com a comercial. L’Anjoli pren el relleu de la Roser com a sòcia corporativa. I seguim comptant també amb tanta altra gent de Porrera i del Priorat, que d’una forma o altra ens presten el seu suport amb el cooperativisme propi de la nostra comarca.
